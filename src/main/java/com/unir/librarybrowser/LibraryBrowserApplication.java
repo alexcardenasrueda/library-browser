aab2b16bc419aaa7ea61f8b5a515ac64b7bbd8eb
Alexander Cardenas <alexcardenas.u@gmail.com>
2023-01-21 08:55:17 -0500

    Wask

---
+++ src/main/java/com/unir/librarybrowser/LibraryBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.unir.librarybrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/librarybrowser/LibraryBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.librarybrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
