d5fbb46780966a025491fc7190e1a3a943780008
alexcardenasrueda <89166772+alexcardenasrueda@users.noreply.github.com>
2023-01-24 18:01:17 -0500

    Feature/books (#3)
    * Update find books functionality
    
    * Update find books
    
    * Format and update notFoundException
    
    * Mysql configuration

--- src/main/java/com/unir/librarybrowser/service/Book.java
+++ src/main/java/com/unir/librarybrowser/service/Book.java
@@ -1,14 +1,14 @@
 package com.unir.librarybrowser.service;
 
 import com.unir.librarybrowser.domain.dto.BookDto;
-import com.unir.librarybrowser.domain.dto.PersonDto;
 import com.unir.librarybrowser.exception.GenericException;
+import com.unir.librarybrowser.exception.NotFoundException;
 
 import java.util.List;
 
 public interface Book {
 
-    public List<BookDto> getAll() throws GenericException;
+    public List<BookDto> getAll() throws GenericException, NotFoundException;
 
-    public BookDto getById(long id);
+    public BookDto getById(long id) throws GenericException, NotFoundException;
 }
--- src/main/java/com/unir/librarybrowser/service/implementation/BookService.java
+++ src/main/java/com/unir/librarybrowser/service/implementation/BookService.java
@@ -1,54 +1,79 @@
 package com.unir.librarybrowser.service.implementation;
 
 import com.unir.librarybrowser.domain.dto.BookDto;
-import com.unir.librarybrowser.domain.dto.PersonDto;
 import com.unir.librarybrowser.domain.entity.BookEntity;
-import com.unir.librarybrowser.domain.entity.PersonEntity;
 import com.unir.librarybrowser.exception.GenericException;
 import com.unir.librarybrowser.exception.NotFoundException;
 import com.unir.librarybrowser.repository.BookRepository;
-import com.unir.librarybrowser.repository.PersonRepository;
 import com.unir.librarybrowser.service.Book;
-import com.unir.librarybrowser.service.Person;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@PropertySource("classpath:constants.properties")
 public class BookService implements Book {
 
-    @Autowired
-    BookRepository repository;
+  @Autowired
+  BookRepository repository;
 
-    @Autowired
-    ModelMapper modelMapper;
+  @Autowired
+  ModelMapper modelMapper;
 
-    /**
-     * @return
-     */
-    @Override
-    public List<BookDto> getAll() throws GenericException {
-        List<BookDto> books = new ArrayList<>();
+  @Value("${exception.book.not_found_all}")
+  private String bookNotFoundAll;
 
-        try {
-            List<BookEntity> entityBooks = repository.findAll();
-            books = modelMapper.map(entityBooks, new TypeToken<List<PersonDto>>() {
-            }.getType());
-            throw new GenericException("Prueba de error 500");
-        } catch (Exception e) {
-            throw new GenericException(e.getMessage());
-        }
+  @Value("${exception.book.not_found}")
+  private String bookNotFound;
+
+  /**
+   * Find all library's books
+   *
+   * @return List of BookDto that contains simple object with book's specification
+   */
+  @Override
+  public List<BookDto> getAll() throws GenericException, NotFoundException {
+    List<BookDto> books = new ArrayList<>();
+
+    try {
+      List<BookEntity> allBooks = repository.findAll();
+      if (allBooks == null || allBooks.isEmpty()) {
+        throw new NotFoundException(bookNotFoundAll);
+      }
+      books = modelMapper.map(allBooks, new TypeToken<List<BookDto>>() {
+      }.getType());
+    } catch (RuntimeException e) {
+      throw new GenericException(e.getMessage());
     }
+    return books;
+  }
 
-    /**
-     * @return
-     */
-    @Override
-    public BookDto getById(long id) {
-        return null;
+  /**
+   * Return one BookDto Object with book's information found by id
+   *
+   * @param id : Long number to find book in DB
+   * @return BookDto object
+   */
+  @Override
+  public BookDto getById(long id) throws GenericException, NotFoundException {
+    BookDto book;
+    try {
+      Optional<BookEntity> entityBook = repository.findById(id);
+      if (entityBook.isEmpty()) {
+        throw new NotFoundException(MessageFormat.format(bookNotFound, id));
+      }
+      book = modelMapper.map(entityBook.get(), BookDto.class);
+    } catch (RuntimeException e) {
+      throw new GenericException(e.getMessage());
     }
+    return book;
+  }
 }
