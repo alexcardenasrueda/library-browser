6024d8930eece530552ddc91635311020e08c355
AnyiBustos <anyi.bustos@mercadolibre.com.co>
2023-01-30 17:47:46 -0500

    Features library (#7)
    * merge develop
    
    * connect ms by name eureka fail
    
    * get person,lend book
    
    * get person,lend book
    
    ---------
    
    Co-authored-by: AnyiBustos <anyi.bustos@mercadoliobre.com.co>

--- src/main/java/com/unir/librarybrowser/service/implementation/LendService.java
+++ src/main/java/com/unir/librarybrowser/service/implementation/LendService.java
@@ -1,6 +1,8 @@
 package com.unir.librarybrowser.service.implementation;
 
+import com.unir.librarybrowser.domain.dto.BookDto;
 import com.unir.librarybrowser.domain.dto.LendDto;
+import com.unir.librarybrowser.domain.entity.BookEntity;
 import com.unir.librarybrowser.domain.entity.LendEntity;
 import com.unir.librarybrowser.exception.GenericException;
 import com.unir.librarybrowser.exception.NotFoundException;
@@ -9,6 +11,7 @@ import com.unir.librarybrowser.service.Lend;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
@@ -25,13 +28,20 @@ public class LendService implements Lend {
     @Autowired
     LendRepository lendRepository;
 
+    @Value("${exception.lend.not_found_all}")
+    private String lendNotFoundAll;
+
+    @Value("${exception.lend.not_found}")
+    private String lendNotFound;
 
     @Override
     public List<LendDto> getAll() throws GenericException {
-
         List<LendDto> lends = new ArrayList<>();
         try {
             List<LendEntity> entityLends = lendRepository.findAll();
+            if (entityLends == null || entityLends.isEmpty()) {
+                throw new NotFoundException(lendNotFoundAll);
+            }
             lends = modelMapper.map(entityLends, new TypeToken<List<LendDto>>() {
             }.getType());
         } catch (Exception e) {
@@ -42,16 +52,16 @@ public class LendService implements Lend {
 
     @Override
     public LendDto getById(long id) throws GenericException,NotFoundException {
-        LendDto lend;
-        try{
-            Optional<LendEntity> lendOpt = lendRepository.findById(id);
-            if (lendOpt.isEmpty()) {
-                throw new NotFoundException(MessageFormat.format("Lend {0} not found", id));
+        LendDto lendDto;
+        try {
+            Optional<LendEntity> lendEntity = lendRepository.findById(id);
+            if (lendEntity.isEmpty()) {
+                throw new NotFoundException(MessageFormat.format(lendNotFound, id));
             }
-            lend = modelMapper.map(lendOpt.get(), LendDto.class);
-        }catch (RuntimeException e){
+            lendDto = modelMapper.map(lendEntity.get(), LendDto.class);
+        } catch (RuntimeException e) {
             throw new GenericException(e.getMessage());
         }
-        return lend;
+        return lendDto;
     }
 }
--- src/main/java/com/unir/librarybrowser/service/implementation/PersonService.java
+++ src/main/java/com/unir/librarybrowser/service/implementation/PersonService.java
@@ -1,48 +1,71 @@
 package com.unir.librarybrowser.service.implementation;
 
+import com.unir.librarybrowser.domain.dto.BookDto;
 import com.unir.librarybrowser.domain.dto.PersonDto;
+import com.unir.librarybrowser.domain.entity.BookEntity;
 import com.unir.librarybrowser.domain.entity.PersonEntity;
 import com.unir.librarybrowser.exception.GenericException;
+import com.unir.librarybrowser.exception.NotFoundException;
 import com.unir.librarybrowser.repository.PersonRepository;
 import com.unir.librarybrowser.service.Person;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonService implements Person {
 
     @Autowired
-    PersonRepository repository;
+    PersonRepository personRepository;
 
     @Autowired
     ModelMapper modelMapper;
 
-    /**
-     * @return
-     */
-    @Override
-    public List<PersonDto> getAll() throws GenericException {
-        List<PersonDto> books = new ArrayList<>();
-        try {
-            List<PersonEntity> entityBooks = repository.findAll();
-            books = modelMapper.map(entityBooks, new TypeToken<List<PersonDto>>() {
-            }.getType());
-        } catch (Exception e) {
-            throw new GenericException(e.getMessage());
-        }
-        return books;
-    }
+    @Value("${exception.person.not_found_all}")
+    private String personNotFoundAll;
+
+    @Value("${exception.person.not_found}")
+    private String personNotFound;
 
     /**
      * @return
      */
     @Override
-    public PersonDto getById(long id) {
-        return null;
+    public List<PersonDto> getAll() throws GenericException, NotFoundException {
+        List<PersonDto> people = new ArrayList<>();
+
+        try {
+            List<PersonEntity> allPeople = personRepository.findAll();
+            if (allPeople == null || allPeople.isEmpty()) {
+                throw new NotFoundException(personNotFoundAll);
+            }
+            people = modelMapper.map(allPeople, new TypeToken<List<PersonDto>>() {
+            }.getType());
+        } catch (RuntimeException e) {
+            throw new GenericException(e.getMessage());
+        }
+        return people;
+    }
+
+    @Override
+    public PersonDto getById(long id) throws GenericException, NotFoundException {
+        PersonDto personDto;
+        try {
+            Optional<PersonEntity> personEntity = personRepository.findById(id);
+            if (personEntity.isEmpty()) {
+                throw new NotFoundException(MessageFormat.format(personNotFound, id));
+            }
+            personDto = modelMapper.map(personEntity.get(), PersonDto.class);
+        } catch (RuntimeException e) {
+            throw new GenericException(e.getMessage());
+        }
+        return personDto;
     }
 }
